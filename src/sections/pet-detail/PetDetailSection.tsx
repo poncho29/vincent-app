7c7b251741db50700751f2de7c122a46faeb51e6
poncho29 <meneses0828@gmail.com>
2024-06-23 11:40:17 -0500

    [ADD] - layout pet detail page desktop

--- src/sections/pet-detail/PetDetailSection.tsx
+++ src/sections/pet-detail/PetDetailSection.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import Image from 'next/image';
 
 import { Button } from '@/components/common';
@@ -27,41 +28,61 @@ export const PetDetailSection = ({ pet }: Props) => {
         alt="Footprints Background"
         width={1240}
         height={210}
-        className='hidden pt-14 pb-16 mx-auto lg:block lg:px-12'
+        className='hidden py-12 mx-auto lg:block'
       />
 
-      <div className='flex flex-col items-center'>
-        <div className='w-full flex items-center mb-5 lg:hidden'>
-          <ArrowBackIcon />
+      <div className='flex flex-col items-center lg:items-start'>
+        <div
+          className='w-full max-w-64 flex items-center mb-5 lg:hidden'
+        >
+          <Link href='/adoptar'>
+            <ArrowBackIcon />
+          </Link>
 
           <h2 className='title grow'>{ pet.name }</h2>
         </div>
 
-        <div className='max-w-64 paragraph'>
+        <div
+          className='max-w-64 paragraph lg:max-w-section lg:flex lg:gap-12'
+        >
           <Image
             src={pet.images[0]}
             alt={pet.name}
             width={240}
             height={240}
-            className='mx-auto mb-5 object-fill'
+            className='mx-auto mb-5 lg:hidden'
+          />
+          
+          <Image
+            src={pet.images[0]}
+            alt={pet.name}
+            width={400}
+            height={400}
+            className='hidden max-h-[400px] lg:block lg:px-0'
           />
 
-          <p className='mb-5'>
-            ¡Hola! me llamo nala, soy tranquila y me encanta caminar
-          </p>
+          <div>
+            <h2 className='hidden title !text-start mb-7 lg:block'>{ pet.name }</h2>
 
-          <div className='flex flex-col items-center gap-1 mb-5'>
-            <DetailInfo detail='raza' value={pet.race} />
-            <DetailInfo detail='sexo' value={pet.sex.sex} />
-            <DetailInfo detail='tamaño' value={pet.size.size} />
-            <DetailInfo detail='etapa' value={pet.stage.stage} />
-            <p>{ pet.sterilized ? 'Esterilizado' : 'No essterilizado' }</p>
-            <p>{ pet.vacine ? 'Vacunado' : 'No vacunado' }</p>
-          </div>
+            <p className='mb-5 lg:mb-7'>
+              ¡Hola! me llamo nala, soy tranquila y me encanta caminar
+            </p>
 
-          <div className='flex justify-center gap-5'>
-            <Button>Adoptar</Button>
-            <Button variant='outline'>Apadrinar</Button>
+            <div
+              className='flex flex-col items-center gap-1 mb-5 lg:items-start lg:mb-7'
+            >
+              <DetailInfo detail='raza' value={pet.race} />
+              <DetailInfo detail='sexo' value={pet.sex.sex} />
+              <DetailInfo detail='tamaño' value={pet.size.size} />
+              <DetailInfo detail='etapa' value={pet.stage.stage} />
+              <p>{ pet.sterilized ? 'Esterilizado' : 'No esterilizado' }</p>
+              <p>{ pet.vacine ? 'Vacunado' : 'No vacunado' }</p>
+            </div>
+
+            <div className='flex justify-center gap-5 lg:justify-start'>
+              <Button>Adoptar</Button>
+              <Button variant='outline'>Apadrinar</Button>
+            </div>
           </div>
         </div>
       </div>
