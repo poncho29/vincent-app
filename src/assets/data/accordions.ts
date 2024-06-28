23279cd8b5b8e8a431a1167721c9f012585879bb
poncho29 <meneses0828@gmail.com>
2024-06-27 19:46:34 -0500

    [ADD] - adding sponsorship FAQs

--- src/assets/data/accordions.ts
+++ src/assets/data/accordions.ts
@@ -3,20 +3,38 @@ import { IAccordion } from "@/interfaces";
 export const initialQuestions: IAccordion[] = [
   {
     id: 1,
-    title: "¿Cuánto cuesta apadrinar un peludo?",
-    text: "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.",
+    title: "¿Qué diferencia hay entre apadrinar y donar?",
+    text: "Apadrinar implica un compromiso financiero regular (mensual) para cubrir las necesidades específicas de un peludito en particular. Por otro lado, donar puede ser una contribución única o recurrente, que se destina a apoyar a la fundación en general. Ambas formas de apoyo son esenciales para mejorar la calidad de vida de los peluditos.",
     status: false
   },
   {
     id: 2,
-    title: "¿Por cuánto tiempo dura el apadrinamiento?",
-    text: "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.",
+    title: "¿Cuánto cuesta apadrinar un peludo?",
+    text: "Puedes elegir el nivel de apoyo que mejor se adapte a ti el cual vas a realizar mes a mes a tu apadrinado.",
     status: false
   },
   {
     id: 3,
-    title: "¿Cual es el proceso de apadrinamiento?",
-    text: "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.",
+    title: "¿Puedo apadrinar más de un peludo?",
+    text: "¡Claro! Puedes apadrinar tantos peludos como desees. Cada apadrinamiento adicional ayuda a más peludos a recibir el cuidado que necesitan.",
+    status: false
+  },
+  {
+    id: 4,
+    title: "¿Qué sucede si el peludo que apadrino es adoptado?",
+    text: "Si el peludo que apadrinas es adoptado, te informaremos y te ofreceremos la opción de transferir tu apadrinamiento a otro peludo que necesite apoyo. Además podrás estar presente en el proceso de adopción de tu apadrinado.",
+    status: false
+  },
+  {
+    id: 5,
+    title: "¿Puedo cancelar mi apadrinamiento en cualquier momento?",
+    text: "Sí, puedes cancelar tu apadrinamiento en cualquier momento. Simplemente contáctanos para gestionar la cancelación.",
+    status: false
+  },
+  {
+    id: 6,
+    title: "¿Donde está ubicada la fundación?",
+    text: "Por motivos de seguridad y bienestar de los peludos, no divulgamos la ubicación exacta de nuestra fundación para evitar abandonos irresponsables en nuestras instalaciones. Sin embargo, cuando se inicia algunos de los procesos de apoyo, puedes solicitar una visita contactándonos directamente.",
     status: false
   }
 ]
